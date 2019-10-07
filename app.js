4bd677518fe9b60f16cd10dd831f175eb5a7c172
Vernet Djama <vernetdjama@gmail.com>
2019-10-07 18:02:04 +0000

    Transformation de l'observable et de la souscription

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ import $ from 'jquery';
 import { fromEvent } from 'rxjs';
 
 var keyUp = fromEvent($('#input'), 'keyup');
+
+var observable = keyUp.pipe(
+  map(e => e.target.value),
+);
+
+observable.subscribe(nb => {
+  console.log(nb);
+}, err => {
+  console.log('Erreur');
+}, completed => {
+  console.log('Fini !');
+});
