0d722cafb39033b0e3b481d401cc83dab737cd46
Vernet Djama <vernetdjama@gmail.com>
2019-10-07 17:41:05 +0000

    Base Structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['./app.js'],
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  }
+};
