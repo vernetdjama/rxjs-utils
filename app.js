95b76c709be2fff77861b4f1a6a6e02fc708fb7c
Vernet Djama <vernetdjama@gmail.com>
2019-10-08 07:58:03 +0000

    Ajout d'un temps de rebond sur l'observable

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var keyUp = fromEvent($('#input'), 'keyup');
 
 var observable = keyUp.pipe(
   map(e => e.target.value),
+  debounceTime(1000),
   map(value => {
     const promise = $.get('http://www.omdbapi.com/?apikey=62c474d4&s=' + encodeURIComponent(value)).promise();
     return from(promise);
