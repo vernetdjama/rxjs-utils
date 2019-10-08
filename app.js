eaef28aa15bf344cba5f1ab1dc6ff8390459f0da
Vernet Djama <vernetdjama@gmail.com>
2019-10-08 07:59:38 +0000

    Ajout d'un systeme d'annulation de doubloun

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var keyUp = fromEvent($('#input'), 'keyup');
 var observable = keyUp.pipe(
   map(e => e.target.value),
   debounceTime(1000),
+  distinctUntilChanged(),
   map(value => {
     const promise = $.get('http://www.omdbapi.com/?apikey=62c474d4&s=' + encodeURIComponent(value)).promise();
     return from(promise);
